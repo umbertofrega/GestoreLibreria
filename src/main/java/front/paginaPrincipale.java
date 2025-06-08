fb3cb66cc4590d13be67de888f57bf2e38c1cf85
umbertofrega <umbertofrega@gmail.com>
2025-06-08 12:07:30 +0200

    migliorato la gui

--- src/main/java/front/paginaPrincipale.java
+++ src/main/java/front/paginaPrincipale.java
@@ -1,6 +1,10 @@
 package front;
 
+import back.Facade;
+import back.Stato;
 import javafx.application.Application;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -9,23 +13,24 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import transfer.Libro;
+
+import java.util.List;
 
 public class paginaPrincipale extends Application {
-    private TableView table = new TableView();
+    private TableView<Libro> table = new TableView();
 
     @Override
     public void start(Stage stage) {
         Scene scene = new Scene(new Group());
         stage.setTitle("PaginaPrincipale");
         table.setEditable(false);
-        TableColumn ISBN = new TableColumn("ISBN");
-        TableColumn Titolo = new TableColumn("Titolo");
-        TableColumn Autore = new TableColumn("Autore");
-        TableColumn valutazione = new TableColumn("Valutazione");
-        TableColumn generi = new TableColumn("Generi");
-        TableColumn statoLettura = new TableColumn("Stato");
 
-        table.getColumns().addAll(ISBN, Titolo, Autore, valutazione, generi, statoLettura);
+        initTable();
+
+        Facade facade = new Facade();
+
+        table.getItems().addAll(facade.getLibri());
 
         Label label = new Label("PaginaPrincipale");
 
@@ -40,6 +45,25 @@ public class paginaPrincipale extends Application {
 
         stage.show();
     }
+
+    private void initTable() {
+        TableColumn<Libro,Integer> ISBN = new TableColumn("ISBN");
+        TableColumn<Libro,String> titolo = new TableColumn("Titolo");
+        TableColumn<Libro,String> autore = new TableColumn("Autore");
+        TableColumn<Libro,Integer> valutazione = new TableColumn("Valutazione");
+        TableColumn<Libro, List<String>> generi = new TableColumn("Generi");
+        TableColumn<Libro, Stato> statoLettura = new TableColumn("Stato");
+
+        titolo.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().titolo()));
+        autore.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().autore()));
+        valutazione.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper<>(cellData.getValue().valutazione()));
+        ISBN.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper<>(cellData.getValue().isbn()));
+        generi.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper<>(cellData.getValue().generi()));
+        statoLettura.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper<>(cellData.getValue().statoLettura()));
+
+        table.getColumns().addAll(ISBN, titolo, autore, valutazione, generi, statoLettura);
+    }
+
 
     public static void main(String[] args) {
         launch();
