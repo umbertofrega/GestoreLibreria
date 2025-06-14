86a80accd92140b34b970af98933ec0127aff020
umbertofrega <umbertofrega@gmail.com>
2025-06-14 09:44:42 +0200

    aggiunte classi per junit test

---
+++ src/test/java/back/gestori/GestoreLibriTest.java
@@ -0,0 +1,26 @@
+package back.gestori;
+
+import org.junit.jupiter.api.Test;
+
+class GestoreLibriTest {
+
+    @Test
+    void inserisciLibro() {
+    }
+
+    @Test
+    void testInserisciLibro() {
+    }
+
+    @Test
+    void rimuoviLibro() {
+    }
+
+    @Test
+    void aggiornaLibro() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+}
---
+++ src/test/java/back/gestori/GestoreRicercheTest.java
@@ -0,0 +1,30 @@
+package back.gestori;
+
+import org.junit.jupiter.api.Test;
+
+class GestoreRicercheTest {
+
+    @Test
+    void getLibri() {
+    }
+
+    @Test
+    void filtraGeneri() {
+    }
+
+    @Test
+    void filtraStato() {
+    }
+
+    @Test
+    void cercaPerAutore() {
+    }
+
+    @Test
+    void cercaPerTitolo() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+}
