661418271300bb5a75277d7f758ae8250cfce434
umbertofrega <umbertofrega@gmail.com>
2025-06-14 10:43:52 +0200

    fatti i test per GestoreRicerche

--- src/test/java/back/gestori/GestoreRicercheTest.java
+++ src/test/java/back/gestori/GestoreRicercheTest.java
@@ -1,30 +1,88 @@
 package back.gestori;
 
+import back.stati.Stato;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import transfer.Libro;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class GestoreRicercheTest {
+    GestoreRicerche gestoreRicerche;
+    List<Libro> libri;
 
-    @Test
-    void getLibri() {
+    @BeforeEach
+    void setUp() {
+        GestoreRicerche gestoreRicerche = GestoreRicerche.getInstance();
+        List<Libro> libri = gestoreRicerche.getLibri();
     }
 
     @Test
-    void filtraGeneri() {
+    @DisplayName("La lista non dovrebbe avere valori null")
+    void testGetLibri() {
+        assertFalse(libri.isEmpty(), "La lista dei libri è vuota");
+        for (Libro libro : libri) {
+            assertNotNull(libro, "Trovato un libro null nella lista");
+        }
     }
 
     @Test
-    void filtraStato() {
+    @DisplayName("I libri all'interno della lista filtrata dovrebbero avere tutti almeno uno degli stati richiesti")
+    void testFiltraStato() {
+        List<Stato> stati = List.of(Stato.DA_LEGGERE);
+        List<Libro> filtrati = gestoreRicerche.filtraStato(libri, stati);
+
+        assertFalse(filtrati.isEmpty(), "Nessun libro filtrato");
+        for (Libro libro : filtrati) {
+            assertTrue(stati.contains(libro.statoLettura()), "Libro con stato sbagliato"+ libro.toString());
+        }
     }
 
     @Test
+    @DisplayName("I libri all'interno della lista filtrata dovrebbero avere almeno uno dei generi richiesti")
+    void testFiltraGenere(){
+        List<String> generiRichiesti = List.of("Romanzo", "Fantascientifico");
+        List<Libro> filtrati = gestoreRicerche.filtraGeneri(gestoreRicerche.getLibri(), generiRichiesti);
+
+        assertFalse(filtrati.isEmpty(), "Nessun libro filtrato");
+
+        for (Libro libro : filtrati) {
+            List<String> genere = libro.generi();
+            boolean almenoUno = false;
+            for (String g : genere) {
+                if (generiRichiesti.contains(g)) {
+                    almenoUno = true;
+                    break;
+                }
+            }
+            assertTrue(almenoUno, "Libro con generi sbagliati: ");
+        }
+    }
+
+
+    @Test
+    @DisplayName("I libri all'interno della lista filtrata dovrebbero avere tutti l'autore richiesto")
     void cercaPerAutore() {
+        List<Libro> filtrati = gestoreRicerche.cercaPerAutore(libri,"Tolkien");
+
+        assertFalse(filtrati.isEmpty(), "Nessun libro filtrato");
+        for (Libro libro : filtrati) {
+            assertEquals("Tolkien", libro.autore(), "Libro con autore errato"+ libro.toString());
+        }
     }
 
     @Test
+    @DisplayName("I libri all'interno della lista filtrata dovrebbero avere tutti il titolo richiesto")
     void cercaPerTitolo() {
+        List<Libro> filtrati = gestoreRicerche.cercaPerTitolo(libri,"Dune");
+
+        assertFalse(filtrati.isEmpty(), "Nessun libro filtrato");
+        for (Libro libro : filtrati) {
+            assertEquals("Dune", libro.titolo(), "Libro con titolo errato" + libro.toString());
+        }
     }
 
-    @Test
-    void getInstance() {
-    }
 }
