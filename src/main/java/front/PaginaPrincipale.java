fd4103164a141b0fb0e35a1edeefe8ddaef33253
umbertofrega <umbertofrega@gmail.com>
2025-06-15 13:39:29 +0200

    tolto responsabilità alla pagina principale

--- src/main/java/front/PaginaPrincipale.java
+++ src/main/java/front/PaginaPrincipale.java
@@ -2,12 +2,13 @@ package front;
 
 import back.Facade;
 import back.stati.Ordinamento;
-import front.dialogFactory.DialogAggiunta;
-import front.dialogFactory.DialogFiltro;
-import front.dialogFactory.DialogModifica;
+import front.bottoni.AggiungiLibroHandler;
+import front.bottoni.BottoneModificaHandler;
+import front.bottoni.EliminaLibroHandler;
+import front.dialogs.AlertPersonale;
+import front.dialogs.DialogFiltro;
+import front.tabella.TabellaPersonale;
 import javafx.application.Application;
-import javafx.beans.property.ReadOnlyObjectWrapper;
-import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -18,13 +19,14 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import transfer.Libro;
+import transfer.LibroValidator;
 
 import javax.swing.*;
 import java.util.List;
 
 public class PaginaPrincipale extends Application {
     private final Facade facade = new Facade();
-    private final TableView<Libro> table = new TableView();
+    private TableView<Libro> table;
 
     public static void main(String[] args) {
         launch();
@@ -36,17 +38,17 @@ public class PaginaPrincipale extends Application {
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
         stage.setTitle("Gestore Libreria");
-        table.setEditable(false);
 
-        initTable();
+        this.table = new TabellaPersonale();
 
         Facade facade = new Facade();
+
+        System.out.println(facade.getLibri());
 
         table.getItems().addAll(facade.getLibri());
 
         final HBox bottoni = boxBottoni();
         Label label = new Label("Gestore Libreria");
-
 
         HBox ricerca = boxRicerca();
 
@@ -57,9 +59,7 @@ public class PaginaPrincipale extends Application {
         VBox.setVgrow(table, Priority.ALWAYS);
 
         root.getChildren().addAll(vbox);
-
         stage.setScene(scene);
-
         stage.show();
     }
 
@@ -115,102 +115,16 @@ public class PaginaPrincipale extends Application {
         return ricerca;
     }
 
-    private void initTable() {
-        table.setMaxWidth(Screen.getPrimary().getVisualBounds().getWidth());
-        table.setMaxHeight(Screen.getPrimary().getVisualBounds().getHeight());
-
-        table.setPrefHeight(Screen.getPrimary().getVisualBounds().getHeight());
-        table.setMaxWidth(Screen.getPrimary().getVisualBounds().getWidth());
-
-        table.setMinWidth(Screen.getPrimary().getVisualBounds().getWidth() / 2);
-        table.setMinHeight(Screen.getPrimary().getVisualBounds().getHeight() / 2);
-
-
-        TableColumn<Libro, Long> ISBN = new TableColumn("ISBN*");
-        TableColumn<Libro, String> titolo = new TableColumn("Titolo*");
-        TableColumn<Libro, String> autore = new TableColumn("Autore*");
-        TableColumn<Libro, Integer> valutazione = new TableColumn("Valutazione");
-        TableColumn<Libro, String> generi = new TableColumn("Generi*");
-        TableColumn<Libro, String> statoLettura = new TableColumn("Stato");
-
-        ISBN.setPrefWidth(table.getMaxWidth() / 10);
-        titolo.setPrefWidth(table.getMaxWidth() / 4);
-        autore.setPrefWidth(table.getMaxWidth() / 5);
-        valutazione.setPrefWidth(table.getMaxWidth() / 12);
-        generi.setPrefWidth(table.getMaxWidth() / 4);
-        statoLettura.setPrefWidth(table.getMaxWidth() / 10.5);
-
-        titolo.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().titolo()));
-        autore.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().autore()));
-        valutazione.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper<>(cellData.getValue().valutazione()));
-        ISBN.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper<>(cellData.getValue().isbn()));
-        generi.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper<>(cellData.getValue().generiString()));
-        statoLettura.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper<>(cellData.getValue().statoLettura().toString()));
-
-
-        table.getColumns().addAll(ISBN, titolo, autore, valutazione, generi, statoLettura);
-    }
-
     private HBox boxBottoni() {
         Button aggiungiLibro = new Button("Aggiungi Libro");
         Button modificaLibro = new Button("Modifica Libro");
         Button eliminaLibro = new Button("Elimina Libro");
 
-        eliminaLibro.setOnAction(e -> {
-            Libro libro = table.getSelectionModel().getSelectedItem();
-            if (libro != null) {
-                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                alert.setTitle("Conferma Eliminazione");
-                alert.getDialogPane().getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
-                alert.setHeaderText("Confermi?");
-                alert.setContentText("Sei sicuro di voler eliminare questo libro?");
-                alert.showAndWait();
-                if (alert.getResult() == ButtonType.OK) {
-                    table.getItems().remove(table.getSelectionModel().getSelectedItem());
-                    facade.rimuoviLibro(libro);
-                }
-            } else {
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.getDialogPane().getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
-                alert.setTitle("Attenzione!");
-                alert.setHeaderText("Ehi...");
-                alert.setContentText("Devi prima selezionare un libro!");
-                alert.show();
-            }
-        });
+        eliminaLibro.setOnAction(new EliminaLibroHandler(table,facade));
 
-        aggiungiLibro.setOnAction(e -> {
-            Dialog<Libro> dialog = new DialogAggiunta().creaDialog();
-            dialog.showAndWait();
-            Libro aggiunta = dialog.getResult();
-            if(aggiunta != null && !esiste(aggiunta)){
-                table.getItems().add(aggiunta);
-                facade.inserisciLibro(aggiunta);
-            }
-        });
+        aggiungiLibro.setOnAction(new AggiungiLibroHandler(table,facade));
 
-        modificaLibro.setOnAction(e -> {
-
-            if (table.getSelectionModel().getSelectedItem() == null) {
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.getDialogPane().getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
-                alert.setTitle("Attenzione!");
-                alert.setContentText("Devi prima selezionare un libro!");
-                alert.show();
-            } else {
-                Libro libroOld = table.getSelectionModel().getSelectedItem().clone();
-                Dialog<Libro> dialog = new DialogModifica(libroOld).creaDialog();
-                dialog.showAndWait();
-                Libro libroNew = dialog.getResult();
-                table.getItems().remove(libroOld);
-                if (libroNew == null || esiste(libroNew)) {
-                    table.getItems().add(libroOld);
-                } else {
-                    table.getItems().add(libroNew);
-                    facade.aggiornaLibro(libroOld, libroNew);
-                }
-            }
-        });
+        modificaLibro.setOnAction(new BottoneModificaHandler(table,facade));
 
         final HBox bottoni = new HBox();
 
@@ -221,15 +135,9 @@ public class PaginaPrincipale extends Application {
     }
 
     private boolean esiste(Libro aggiunta) {
-        for (Libro l : table.getItems()) {
-            if (l.isbn() == aggiunta.isbn()) {
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.getDialogPane().getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
-                alert.setHeaderText("Ehi!");
-                alert.setContentText("Un libro con questo codice ISBN è già esistente...");
-                alert.showAndWait();
-                return true;
-            }
+        if(!LibroValidator.esisteISBN(aggiunta,table.getItems())) {
+            new AlertPersonale(Alert.AlertType.ERROR,"Attento","Ehi!","Un libro con questo codice ISBN è già esistente...").showAndWait();
+            return true;
         }
         return false;
     }
